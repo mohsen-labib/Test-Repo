ece533e069ddab1d915046fdf20a5b13dd526b46
mohsen <mohsen@Mohsen>
2021-02-26 21:17:56 +0300

    First

---
+++ ERP-/src/com/tic/erp/Main.java
@@ -0,0 +1,27 @@
+package com.tic.erp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "STU", "STU");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from TEST");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString(2));
+			}
+			System.out.println("Coonected");
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}

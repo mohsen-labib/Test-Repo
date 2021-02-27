b0a66c2ff9e9a04df246b95a07b733c905769fcb
mohsen <mohsen@Mohsen>
2021-02-27 11:12:21 +0300

    test

--- ERP-/src/com/tic/erp/Main.java
+++ ERP-/src/com/tic/erp/Main.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
-
+//mmm
 	public static void main(String[] args) {
 		try {
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "STU", "STU");
